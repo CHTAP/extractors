94aaaf7f867ab673949e200dc6543a80f7fdba31
jdunnmon <jdunnmon@gmail.com>
2018-03-01 11:03:39 -0800

    intermediate dev

--- set_env.sh
+++ set_env.sh
@@ -1,3 +1,3 @@
-export SNORKELHOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd ../backup/snorkel && pwd )"
-export FONDUERHOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd ../backup/snorkel/snorkel/contrib/fonduer && pwd )"
+export SNORKELHOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd ../snorkel && pwd )"
+export FONDUERHOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd ../snorkel/snorkel/contrib/fonduer && pwd )"
 source $FONDUERHOME/set_env.sh
