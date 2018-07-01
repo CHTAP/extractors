c11b2d4e791569d5032542c2d49d1d06513c7328
jdunnmon <jdunnmon@gmail.com>
2018-07-01 12:39:14 -0700

    updated NB1

--- set_env.sh
+++
@@ -1,3 +0,0 @@
-export SNORKELHOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd ../snorkel && pwd )"
-export FONDUERHOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd ../snorkel/snorkel/contrib/fonduer && pwd )"
-source $FONDUERHOME/set_env.sh
