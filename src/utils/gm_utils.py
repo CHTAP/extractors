ac466351ed737b106781ad053dd38fb6d137df65
Saeideh Shahrokh Esafahni <saeideh@raiders6.stanford.edu>
2018-07-25 23:52:42 -0700

    Adding a function to gm_utils for creating dict phone

--- src/utils/gm_utils.py
+++ src/utils/gm_utils.py
@@ -101,3 +101,109 @@ def create_extractions_dict(session, cands, train_marginals, extractions, dummy=
             doc_extractions[doc_name]['dummy'].append('dummy_ext')
         
     return doc_extractions
+#####################################################################
+def phone_cleaning (c):
+    """ cleaning a candidate which has punctuations or letters, example: '8.3.2.8.9.7.8.2.1.0.&nbsp;Call' or '4143058071\\\\n'"""
+    phone = re.sub("[^0-9]","", c)
+    return phone
+
+def word_to_number(span_input):
+    num_dict = {"one":"1", 'two':"2", 'three':"3",'four':"4",'five':"5",'six':"6",'seven':"7",'eight':'8','nine':"9",'ten':'10'}
+    
+    for nb in ['one', 'two', 'three','four','five','six','seven','eight','nine','ten']:
+        if span_input.find(nb):
+            span_input = span_input.replace(nb,num_dict[nb] ).replace(" ","")
+    return span_input
+        
+def count_(span_input, pattern):
+    count = 0
+    while len(span_input)>0:
+        idx = span_input.find(pattern) # returns first position of character matching pattern
+        span_input = span_input[idx+len(pattern):]
+        if idx<0:
+            break
+        else:
+            count+=1
+    return count
+       
+def PhoneNumber( number ):
+    areaCode = number[0:3 ]
+    exchange = number[3:6 ]
+    line = number[6:] 
+    return "(%s) %s-%s" % ( areaCode, exchange, line )
+
+def arrange_phone(p):
+    if len(p)==10:
+        return PhoneNumber(p)
+    if len(p)==11:
+        return PhoneNumber(p[1:])
+    if len(p) == 13:
+        return PhoneNumber(p[3:])
+    else:
+        return p
+    
+def phone_eval(phone):
+    for nb in ['one', 'two', 'three','four','five','six','seven','eight','nine','ten']:
+        if count_(phone,nb)!=0:
+            return arrange_phone(phone_cleaning(word_to_number(phone)))
+    
+    
+    
+    if phone.isdigit():
+        result = arrange_phone(phone)
+        return result
+    else:
+        phone = phone_cleaning(phone)
+        if phone.isdigit():
+            result = arrange_phone(phone)
+            return result
+        else:
+            #phone =[]
+            #return []
+            return phone
+######################################################################################################
+##### HELPER FUNCTIONS FOR EXTRACTIONS
+######################################################################################################
+
+def create_phone_extractions_dict(session, cands, train_marginals, extractions, dummy=False):
+    """
+    Creating dictionary of extractions from label matrix and marginals.
+    
+    session: DB connection
+    LabelMatrix or List cands: training label matrix
+    list(int) train_marginals: marginal probablities emitted from Snorkel
+    list extractions: list of extractions to add
+    bool dummy: include dummy extraction
+    """
+    
+    doc_extractions = {}
+    num_train_cands = max(train_marginals.shape)
+    train_cand_preds = (train_marginals>0.5)*2-1
+    for ind in range(num_train_cands):
+        if type(cands) == list:
+            cand = cands[ind]
+        else:
+            cand = cands.get_candidate(session,ind)
+        parent = cand.get_parent()
+        url = parent.document.meta['url']
+        doc_name = parent.document.name
+   
+        # Initializing key if it doesn't exist
+        if doc_name not in doc_extractions.keys():
+            doc_extractions[doc_name] = {}
+            doc_extractions[doc_name]['url'] = url
+            for extraction in extractions:
+                doc_extractions[doc_name][extraction] = []
+            if dummy:
+                doc_extractions[doc_name]['dummy'] = []
+        
+        # Adding extraction to extractions dict if prediction == 1
+        if train_cand_preds[ind] == 1:
+            for extraction in extractions:
+                ext = phone_eval(getattr(cand,extraction).get_span().lower())
+                #clean_ext = ext(phone_eval(ext))
+                doc_extractions[doc_name][extraction].append(ext)
+        if dummy:
+            doc_extractions[doc_name]['dummy'].append('dummy_ext')
+        
+    return doc_extractions
