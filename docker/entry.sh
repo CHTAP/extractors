20872e77eb1639c166ca2589b8c92ed09068ab3f
Jared <jdunnmon@gmail.com>
2018-12-24 05:04:35 +0000

    changing entrypoint to have arguments

--- docker/entry.sh
+++ docker/entry.sh
@@ -13,7 +13,8 @@ sudo /etc/init.d/postgresql restart
 
 # PUT CODE HERE FOR RUNNING EXTRACTORS!
 echo "Running extractors"
-bash /home/repos/extractors/docker/run_extractors.sh /data/input.csv /home/repos/extractors/docker/run_config.json
+#bash /home/repos/extractors/docker/run_extractors.sh /data/input.csv /home/repos/extractors/docker/run_config.json
+bash /home/repos/extractors/docker/run_extractors.sh $1 $2
 
 # COMPLETE!
 echo "Extraction complete"
