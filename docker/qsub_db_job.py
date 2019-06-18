72240a4701881c8302b0fe96ab66c8a768ef736a
jdunnmon <jdunnmon@gmail.com>
2019-06-18 15:10:32 -0700

    add check empty extraction

--- docker/qsub_db_job.py
+++ docker/qsub_db_job.py
@@ -5,6 +5,7 @@ parser.add_argument('--input', required=True,
                     help='directory containing pre-parsed DBs')
 parser.add_argument('--logdir', required=True, help='default log path')
 parser.add_argument('--outdir', required=True, help='default output path')
+parser.add_argument('--check', default=None)
 args = parser.parse_args()
 
 if not os.path.exists(args.outdir):
@@ -25,10 +26,12 @@ files = [os.path.join(root, name)
 for flpth in sorted(files):
     if flpth.endswith('-db'):
         fl = os.path.split(flpth)[-1]
-        #respath = os.path.join(args.outdir,'price_per_hour','price_per_hour_extraction_'+fl+'.jsonl')
-        respath = os.path.join(args.outdir,'phone','phone_extraction_'+fl+'.jsonl')
+        if args.check is not None:
+            respath = os.path.join(args.outdir,'args.check',f'{args.check}_extraction_'+fl+'.jsonl')
+        else:
+            respath = '3!@#$%^^' #dummy path which will never be found
+        #respath = os.path.join(args.outdir,'phone','phone_extraction_'+fl+'.jsonl')
         flpth = flpth
-        #if True:
         if not os.path.exists(respath):
             cmd = "qsub -V -b y -r y -N job-{} -wd {} 'sh /dfs/scratch0/jdunnmon/chtap/extractors/docker/run_extractors_docker_local_dfs_fonduer.sh {} {}'".format(fl, args.logdir, flpth, args.outdir)
             os.system(cmd)
